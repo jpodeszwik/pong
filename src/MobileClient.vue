ade9dbeccbe187f125738518c3025e21df42fb54
damienix <damienix1@gmail.com>
2017-09-03 17:22:33 +0200

    Add Vuetify to make mobile view look much better

--- src/MobileClient.vue
+++ src/MobileClient.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>Jestem na fonie</div>
+  <v-container>
+    <h2>Podaj imię</h2>
+
+    <v-layout>
+      <v-flex xs12>
+        <v-text-field
+            label="Imię"
+            :value="name"
+        ></v-text-field>
+        <v-btn primary>Dołącz</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
   export default {
+    data() {
+      return {name: ''}
+    },
+
     mounted() {
     }
   }
 </script>
+
+<style scoped>
+</style>
